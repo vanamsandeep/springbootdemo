7b78b727f4fb5076e50042f203e5667a51719dfe
vanamsandeep <sandeepvanam58@gmail.com>
2024-04-30 23:40:36 -0400

    Initial spring boot demo

---
+++ src/test/java/springboot/practise/springbootdemo/SpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.practise.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
